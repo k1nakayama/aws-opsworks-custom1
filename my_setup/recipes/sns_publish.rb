2f09541f9e841b89e38f30e99cefce29f722eeea
Keiichi Nakayama <nakayama@chara-web.co.jp>
2015-07-31 18:31:33 +0900

    sns_publish.rb

--- my_setup/recipes/sns_publish.rb
+++ my_setup/recipes/sns_publish.rb
@@ -1,5 +1,5 @@
 bash "sns_publish" do
   code <<-EOC
-    aws sns publish --region ap-northeast-1 --topic-arn "arn:aws:sns:ap-northeast-1:949856713186:ow-aleart" --message "instance started"
+    aws sns publish --topic-arn "arn:aws:sns:ap-northeast-1:949856713186:ow-aleart" --message "instance started"
   EOC
 end
