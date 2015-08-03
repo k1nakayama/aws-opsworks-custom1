ce15f75b14217e55640572f850f3afa9c0eb3b62
Keiichi Nakayama <nakayama@chara-web.co.jp>
2015-08-03 17:42:12 +0900

    sns_publish.rb

--- my_setup/recipes/sns_publish.rb
+++ my_setup/recipes/sns_publish.rb
@@ -1,3 +1,3 @@
 bash "sns_publish" do
-  code 'aws sns publish --topic-arn "arn:aws:sns:ap-northeast-1:949856713186:ow-aleart" --message "instance started \n hostname: ' + node[:opsworks][:instance][:hostname] + '\n ip: ' + node[:opsworks][:instance][:private_ip] + '"'
+  code 'aws sns publish --topic-arn "arn:aws:sns:ap-northeast-1:949856713186:ow-aleart" --message "instance started  hostname: ' + node[:opsworks][:instance][:hostname] + ' ip: ' + node[:opsworks][:instance][:private_ip] + '"'
 end
