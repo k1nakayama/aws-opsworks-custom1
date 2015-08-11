4f5bef102a4b28de92e11ec8efe72fdb2f940662
Keiichi Nakayama <nakayama@chara-web.co.jp>
2015-08-11 14:44:35 +0900

    my_setup/recipes/sns_publish.rb my_shutdown/recipes/sns_publish.rb

---
+++ my_shutdown/recipes/sns_publish.rb
@@ -0,0 +1,3 @@
+bash "sns_publish" do
+  code 'aws sns publish --topic-arn "arn:aws:sns:ap-northeast-1:949856713186:ow-aleart" --message "instance shutdown  stack: '+ node[:opsworks][:stack][:name] + ' hostname: ' + node[:opsworks][:instance][:hostname] + ' ip: ' + node[:opsworks][:instance][:private_ip] + '"'
+end
