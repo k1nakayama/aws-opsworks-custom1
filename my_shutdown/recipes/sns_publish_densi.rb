19646ecf5bf53a15b9115f5a08688e481233d272
Keiichi Nakayama <nakayama@chara-web.co.jp>
2015-08-20 16:56:11 +0900

    sns_publish_densi.rb

---
+++ my_shutdown/recipes/sns_publish_densi.rb
@@ -0,0 +1,3 @@
+bash "sns_publish" do
+  code 'aws sns publish --topic-arn "arn:aws:sns:ap-northeast-1:461461241914:ow-aleart" --message "instance shutdown  stack: '+ node[:opsworks][:stack][:name] + ' hostname: ' + node[:opsworks][:instance][:hostname] + ' ip: ' + node[:opsworks][:instance][:private_ip] + '"'
+end
