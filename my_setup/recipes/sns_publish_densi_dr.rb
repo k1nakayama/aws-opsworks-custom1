e134d8c8155322b1f1bc3279a5bda61c56103eb4
Keiichi Nakayama <nakayama@chara-web.co.jp>
2015-09-10 18:08:40 +0900

    sns_publish_densi_dr.rb

---
+++ my_setup/recipes/sns_publish_densi_dr.rb
@@ -0,0 +1,3 @@
+bash "sns_publish" do
+  code 'aws sns publish --topic-arn "arn:aws:sns:us-west-2:461461241914:dr-ow-aleart" --message "instance started stack: '+ node[:opsworks][:stack][:name] + ' hostname: ' + node[:opsworks][:instance][:hostname] + ' ip: ' + node[:opsworks][:instance][:private_ip] + '"'
+end
